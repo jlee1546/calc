f2f4d806fc160cac4a5d94fe0224a3ae83a6e724
Jonathan Lee <jlee1546.jl@gmail.com>
2022-07-24 13:14:38 -0400

    Add writeNumber function and eventlisteners to numbers keys

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 // script for calculator project
 
+//global variabes needed for operation
+let digit1, digit2, operator, flag;
+
 // functions for the math operations
 
 function add(a, b) {
-  return a + b;
+  console.log(a + b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  console.log(a - b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  console.log(a * b);
 }
 
 function divide(a, b) {
-  return a / b;
+  console.log(a / b);
 }
 
 // functions for operations
 
-function clear() {}
+function clear() {
+  const screen = document.querySelector(".screen > div");
+  screen.textContent = "0";
+}
 
-function operate(operator, a, b) {}
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+  }
+}
 
 function decimal() {}
+
+function writeNumber(e) {
+  const screen = document.querySelector(".screen > div");
+  screen.textContent += e.target.id;
+}
+
+//event listeners
+
+// event listner for numbers
+const digitKeys = document.querySelectorAll(".number");
+
+digitKeys.forEach((digit) => {
+  digit.addEventListener("click", writeNumber);
+});
