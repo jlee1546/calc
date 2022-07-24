51e2fac1372f9e4a4ab71c745a8da146b8875dbe
Jonathan Lee <jlee1546.jl@gmail.com>
2022-07-24 11:17:20 -0400

    Add basic math and operator functions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // script for calculator project
+
+// functions for the math operations
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// functions for operations
+
+function clear() {}
+
+function operate(operator, a, b) {}
+
+function decimal() {}
